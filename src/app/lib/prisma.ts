2ad6fa469dadb83b047148db295711a17a1059bc
Rabbi Khan <Krabby2244@gmail.com>
2026-09-22 01:43:58 +0600

    fix(build): clean dist folder before build and fallback DATABASE_URL handling in prisma lib

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import config from "../config/index.js";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL || config.database_url;
+
+if (!connectionString) {
+	console.error("CRITICAL: DATABASE_URL environment variable is missing!");
+}
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
